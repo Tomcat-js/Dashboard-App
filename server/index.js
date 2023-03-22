39f3e3a4ba6b016d0dcd8e97e8de4fb88cb03ada
Thomas Catchlove <thomascatchlove.js@gmail.com>
2023-03-22 12:32:48 +1100

    create wall art MVP

--- server/index.js
+++ server/index.js
@@ -5,34 +5,78 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-let url =
-  "https://api.artic.edu/api/v1/artworks/search?q=impressionism&limit=10";
+// let testUrl = `https://api.artic.edu/api/v1/artworks/search?q=romanticism&limit=20`;
 
-let imageIdArr = [];
+// let imageIdArr = [];
 
-axios
-  .get(url)
-  .then((res) => {
-    // console.log("axios called");
-    const requests = res.data.data.map((url) => axios.get(url.api_link));
-    axios.all(requests).then((responses) => {
-      responses.forEach((resp) => {
-        imageIdArr.push(resp.data.data.image_id);
+const callApi = (style) => {
+  let imageIdArr = [];
+  return axios
+    .get(`https://api.artic.edu/api/v1/artworks/search?q=${style}&limit=20`)
+    .then(async (res) => {
+      console.log("axios called");
+      const requests = res.data.data.map((url) => axios.get(url.api_link));
+      await axios.all(requests).then((responses) => {
+        responses.forEach((resp) => {
+          imageIdArr.push(resp.data.data.image_id);
+        });
       });
-    });
-  })
-  .catch((err) => {
-    console.log("Error: ", err.message);
-  });
 
-app.get("/artwork", (req, res) => {
-  res.json({
-    idArr: imageIdArr,
-  });
-});
+      return imageIdArr;
+    })
+    .catch((err) => {
+      console.log("Error: ", err.message);
+    });
+};
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get("/pop_art", async (req, res) => {
+  let popArtArray = await callApi("pop art");
+  res.json({
+    idArr: popArtArray,
+  });
+});
+
+app.get("/surrealism", async (req, res) => {
+  let surrealismArray = await callApi("surrealism");
+  res.json({
+    idArr: surrealismArray,
+  });
+});
+
+app.get("/impressionism", async (req, res) => {
+  let impressionismArray = await callApi("impressionism");
+  res.json({
+    idArr: impressionismArray,
+  });
+});
+
+app.get("/romanticism", async (req, res) => {
+  let romanticismArray = await callApi("romanticism");
+  res.json({
+    idArr: ["badimage", ...romanticismArray],
+  });
+});
+
+app.get("/expressionism", async (req, res) => {
+  try {
+    let expressionismArray = await callApi("expressionism");
+    res.json({
+      idArr: expressionismArray,
+    });
+  } catch (error) {
+    console.error(err.response.data);
+  }
+});
+
+app.get("/rococo", async (req, res) => {
+  let rococoArray = await callApi("rococo");
+  res.json({
+    idArr: rococoArray,
+  });
 });
 
 app.listen(PORT, () => {
